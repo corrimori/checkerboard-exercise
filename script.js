a775f6b299eeac145953b530776d3d7ce3fb3e2b
Corrine Morita <corrine@promptt.com>
2018-04-27 13:31:59 -0700

    updated js file to create random colors

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
-// Your JS goes here
+'use strict';
+
+var body = document.getElementsByTagName('body')[0];
+
+let makeRandomColor = function() {
+  let randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+}
+
+for (var j = 0; j < 8; j++) {
+    // draws ODD row, start with red square
+    if (j % 2 !== 0) {
+      for (var i = 0; i < 8; i++) {
+        var div = document.createElement('div');
+        var randomColor = "#"+((1<<24)*Math.random()|0).toString(16)
+
+        div.style.float = 'left';
+        div.style.width = '12.5%';
+        div.style.paddingBottom = '12.5%';
+
+        if (i % 2 === 0) {
+          div.style.backgroundColor = randomColor;
+        } else {
+          div.style.backgroundColor = randomColor;
+        }
+        body.appendChild(div);
+      }
+    } else { // draws EVEN row, start with black square
+      for (var i = 0; i < 8; i++) {
+        var div = document.createElement('div');
+        var randomColor = "#"+((1<<24)*Math.random()|0).toString(16)
+
+        div.style.float = 'left';
+        div.style.width = '12.5%';
+        div.style.paddingBottom = '12.5%';
+
+        if (i % 2 === 0) {
+          div.style.backgroundColor = randomColor;
+        } else {
+          div.style.backgroundColor = randomColor;
+        }
+        body.appendChild(div);
+      }
+    }
+  }
+
+
+  // generate a random color
+  // let randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+  // console.log(randomColor);
+  // '#'+Math.random().toString(16).substr(-6);
+
+
+//
+// } else { // draws EVEN row, start with black square
+//   for (var i = 0; i < 8; i++) {
+//     var div = document.createElement('div');
+//
+//     div.style.float = 'left';
+//     div.style.width = '12.5%';
+//     div.style.paddingBottom = '12.5%';
+//
+//     if (i % 2 === 0) {
+//       div.style.backgroundColor = 'black';
+//     } else {
+//       div.style.backgroundColor = 'red';
+//     }
+//     body.appendChild(div);
+//   }
+// }
+// }
