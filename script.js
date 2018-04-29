d38c95098367d3f4e8805f0278976b669a66f3d3
Corrine Morita <corrine@promptt.com>
2018-04-29 13:52:15 -0700

    version to add flashing random colors

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-//black and white version
+// FLASHING COLORS Version
+// On a new branch called `flashing`, write code to change each tile's color
+// to a new random color every 2 seconds.
+
 
 'use strict';
 
 var body = document.getElementsByTagName('body')[0];
-// body.style.background = 'linear-gradient(45deg, blue, red)';
 
 for (var j = 0; j < 8; j++) {
     // draws ODD row, start with red square
     if (j % 2 !== 0) {
       for (var i = 0; i < 8; i++) {
         var div = document.createElement('div');
-        var randomColor = "#"+((1<<24)*Math.random()|0).toString(16)
+        var randomColor = '#'+((1<<24)*Math.random()|0).toString(16)
 
         div.style.float = 'left';
         div.style.width = '12.5%';
         div.style.paddingBottom = '12.5%';
 
         if (i % 2 === 0) {
-          div.style.backgroundColor = 'red';
+          div.style.backgroundColor = randomColor;
         } else {
-          div.style.backgroundColor = 'black';
+          div.style.backgroundColor = randomColor;
         }
         body.appendChild(div);
       }
     } else { // draws EVEN row, start with black square
       for (var i = 0; i < 8; i++) {
         var div = document.createElement('div');
-        var randomColor = "#"+((1<<24)*Math.random()|0).toString(16)
+        var randomColor = '#'+((1<<24)*Math.random()|0).toString(16)
 
         div.style.float = 'left';
         div.style.width = '12.5%';
         div.style.paddingBottom = '12.5%';
 
         if (i % 2 === 0) {
-          div.style.backgroundColor = 'black';
+          div.style.backgroundColor = randomColor;
         } else {
-          div.style.backgroundColor = 'red';
+          div.style.backgroundColor = randomColor;
         }
         body.appendChild(div);
       }
     }
   }
+
+// function to change each square to random color
+function flash() {
+  for (let i = 0; i < body.children.length; i++) {
+    let randomColor = '#'+((1<<24)*Math.random()|0).toString(16);
+    console.log(body.children.length);
+    body.children[i].style.backgroundColor = randomColor;
+  }
+}
+
+setInterval(flash, 2000);
